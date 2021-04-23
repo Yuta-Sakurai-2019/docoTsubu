f817f28dfe2babba04eb6e14131591fe73c74c8e
Yuta-Sakurai-2019 <mogle1989@gmail.com>
2021-04-23 18:55:48 +0900

    つぶやき一覧、投稿機能add

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 @WebServlet("/Main")
@@ -51,6 +52,36 @@ public class Main extends HttpServlet {
 			dispatcher.forward(request, response);
 		}
 		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		
+		//入力値チェック
+		if(text != null && text.length() != 0) {
+			//アプリケーションスコープに保存されたつぶやきリスト取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List <Mutter>) application.getAttribute("mutterList");
+			
+			//セッションスコープに保存されたユーザー情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+			
+			//つぶやきをつぶやきリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(),text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+			
+			//アプリケーションスコープにつぶやきリストを保存
+			application.setAttribute("mutterList", mutterList);
+		}
+		
+		//メイン画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
+	
 	}
 
 }
