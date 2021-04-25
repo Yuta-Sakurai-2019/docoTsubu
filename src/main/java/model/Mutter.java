63a7a195b16ef19fc48f7619f33553e748a68af4
Yuta-Sakurai-2019 <mogle1989@gmail.com>
2021-04-25 17:48:25 +0900

    idゲッター追加

--- src/main/java/model/Mutter.java
+++ src/main/java/model/Mutter.java
@@ -2,14 +2,21 @@ package model;
 
 public class Mutter {
 
+	private int id;
 	private String userName; //ユーザー名
 	private String text; //投稿内容
+	
 	public Mutter() {
-		
+		//何もしない
 	}
-	public Mutter(String userName, String text) {
+	public Mutter(int id, String userName, String text) {
+		this.id = id;
 		this.userName = userName;
 		this.text = text;
+	}
+	
+	public int getId() {
+		return id;
 	}
 	
 	public String getUserName() {
